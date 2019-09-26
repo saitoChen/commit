da61d0afe61296586ac22fe7cd0575cad953d94f
陈剑峰 <19054514@sn.suning.ad>
2019-09-26 15:54:36 +0800

    测试

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,4 +1,33 @@
 module.exports = {
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^(\w*|[\u4e00-\u9fa5]*)(?:[\(\（](.*)[\)\）])?[\:\：] (.*)/,
+            headerCorrespondence: [
+            'type',
+            'scope',
+            'subject'
+            ],
+            referenceActions: [
+            'close',
+            'closes',
+            'closed',
+            'fix',
+            'fixes',
+            'fixed',
+            'resolve',
+            'resolves',
+            'resolved'
+            ],
+            issuePrefixes: ['#'],
+            noteKeywords: ['BREAKING CHANGE', '不兼容变更'],
+            fieldPattern: /^-(.*?)-$/,
+            revertPattern: /^Revert\s"([\s\S]*)"\s*This reverts commit (\w*)\./,
+            revertCorrespondence: ['header', 'hash'],
+            warn() {},
+            mergePattern: null,
+            mergeCorrespondence: null
+        },
+    },
     rules: {
       'type-enum': [2, 'always', ['新功能', '修复', '优化', '重构', '文档', 'test', 'chore', 'revert', 'WIP', 'build', 'release']],
       'type-case': [0],
